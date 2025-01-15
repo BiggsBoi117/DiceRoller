d1eb39aae67972dc1073e78f3e8ac947c49a0988
Ethan Biggs <ethanscott402@gmail.com>
2025-01-15 09:35:51 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DiceRoller;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        System.Console.WriteLine("Dice sim loading...");
+        Initialize();
+        int rolls = Program.UserInput();
+        roller.RollDice(rolls);
+
+    }
+
+    private static int UserInput()
+    {
+        while (true)
+        {
+            System.Console.WriteLine("How many rolls?");
+            try
+            {
+                int rolls = int.Parse(Console.ReadLine());
+                return rolls;
+            }
+            catch
+            {
+                System.Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+
+    private static void Initialize()
+    {
+        try
+        {
+            roller TestRoller = new roller();
+            System.Console.WriteLine("Welcome to the dice roller!");
+        }
+        catch (Exception e)
+        {
+            System.Console.WriteLine("Error loading dice roller: " + e.Message);
+        }
+    }
+}
---
+++ roller.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller
+{
+    internal class roller
+    {
+        private int rolls;
+
+        public roller() { rolls = 0; }
+
+        public static Array RollDice(int rolls)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                int die1 = new Random().Next(1, 7);
+                int die2 = new Random().Next(1, 7);
+                int dieResult = die1 + die2;
+                Console.WriteLine(dieResult);
+            }
+            return null;
+        }
+    }
+}
